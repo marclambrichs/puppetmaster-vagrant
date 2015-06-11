8514a2e8e8cee929bf85512a3da2f0c0fb21c506
Marc Lambrichs <mlambrichs@melange-it.nl>
2015-06-11 05:25:05 +0200

    Changed remotes

--- puppet/manifests/r10k.pp
+++ puppet/manifests/r10k.pp
@@ -1,15 +1,15 @@
 node default {
   class { 'r10k':
-    remote    => 'git@cassandra.melange-it.nl:/var/opt/git/generic/r10k.git',
+    remote    => 'https://github.com/mlambrichs/arthurjames-r10k.git',
     sources   => {
       'puppet' => {
         'basedir' => "${::settings::confdir}/environments",
-        'remote'  => 'git@cassandra.melange-it.nl:/var/opt/git/generic/r10k.git',
+        'remote'  => 'https://github.com/mlambrichs/arthurjames-r10k.git',
         'prefix'  => false
       },
       'hiera' => {
-        'basedir' => "/var/lib/hiera",
-        'remote'  => 'git@cassandra.melange-it.nl:/var/opt/git/generic/hiera.git',
+        'basedir' => '/var/lib/hiera',
+        'remote'  => 'https://github.com/mlambrichs/arthurjames-hiera.git',
         'prefix'  => false
       }
     }
